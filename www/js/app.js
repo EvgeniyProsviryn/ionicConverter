21e1d4551e76aa063bd3d04e6cde9c62bafc20a4
Evgeniy Prosviryn <start_up_project@mail.ru>
2017-10-26 01:21:28 +0300

    1.1.1

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,28 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('converter', ['ionic']);
 
-.run(function($ionicPlatform) {
+app.controller('converterCtrl', ['$scope','$http', function(scope,http){
+  scope.curs = {};
+  scope.result = 0;
+  http({
+    method: "GET",
+    url: "https://www.cbr-xml-daily.ru/daily_json.js"
+  }).then(function(result){
+    scope.curs = result.data;
+  })
+  scope.convert = function(amount,baseSelect,convertSelect){
+    if(baseSelect&&convertSelect&&amount){
+      var result = (scope.curs.Valute[baseSelect].Value/scope.curs.Valute[convertSelect].Value)*amount;
+      scope.result = result.toFixed(2);
+    }else{
+      scope.result = 0;
+    }
+  }
+}])
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
